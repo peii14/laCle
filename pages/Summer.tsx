a9e7b50c7fa570dcd0923e50c54e304cd38b6892
Gayuh kautaman <gkoma444@gmail.com>
2022-07-22 14:28:13 +0700

    fixed position

--- pages/Summer.tsx
+++ pages/Summer.tsx
@@ -9,12 +9,14 @@ const Summer = () => {
     <div className="layout">
       <div className="h-screen overflow-hidden mt-2">
         <Hero content="" subTitle="" title="Summer" image={summer} />
-        <div className="border-x-2 h-full z-0 border-forth border-opacity-60 grid grid-cols-3  ">
+        <div className="border-x-2 h-1/4 border-forth border-opacity-60 grid grid-cols-3  ">
           <div className="col-start-2 border-x-2 border-forth border-opacity-60 relative ">
-            <div className="absolute z-0">
+            <div className="absolute top-1/4 -translate-y-1/2 left-2 ">
               <Image alt="dot" src={dot} height={50} width={50} />
             </div>
-            <h1 className="text-center text-forth z-10">Top Activities</h1>
+            <h1 className="text-center whitespace-nowrap text-forth z-10 absolute left-1/2 -translate-x-1/2 top-1/3 -translate-y-1/2">
+              Top Activities
+            </h1>
           </div>
         </div>
       </div>
