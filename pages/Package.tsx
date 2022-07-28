4034f62a8f3bcc4836d0ad48b172269026b2708a
Gayuh kautaman <gkoma444@gmail.com>
2022-07-28 13:58:30 +0700

    added packages

---
+++ pages/Package.tsx
@@ -0,0 +1,13 @@
+import Hero from "../components/Hero";
+import paris from "../images/package/paris.png";
+const Package = () => {
+  return (
+    <div className="layout">
+      <div className="h-screen mt-2">
+        <Hero content="" title="Packages" image={paris} />
+      </div>
+      <div></div>
+    </div>
+  );
+};
+export default Package;
