dfd085b3b6ae92871a06688eac6dd9637408a50a
Gayuh kautaman <gkoma444@gmail.com>
2022-07-30 18:39:14 +0700

    fixed navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,22 +4,22 @@ const Navbar = () => {
   return (
     <nav className="text-center flex flex-row gap-5 justify-center py-5 text-forth">
       <Link href="/">
-        <a className="duration-500 hover:scale-110">
+        <a className="duration-500 bg-opacity-60 hover:scale-110 hover:bg-sec rounded-sm hover:px-5 hover:text-primary">
           <p>Home</p>
         </a>
       </Link>
       <Link href="/Package">
-        <a>
+        <a className="duration-500 hover:scale-110 hover:bg-sec rounded-sm hover:px-5 hover:text-primary">
           <p>Package</p>
         </a>
       </Link>
       <Link href="/Booking">
-        <a>
+        <a className="duration-500 hover:scale-110 hover:bg-sec rounded-sm hover:px-5 hover:text-primary">
           <p>Book Your Plan</p>
         </a>
       </Link>
       <Link href="/Team">
-        <a>
+        <a className="duration-500 hover:scale-110 hover:bg-sec rounded-sm hover:px-5 hover:text-primary">
           <p>Team</p>
         </a>
       </Link>
