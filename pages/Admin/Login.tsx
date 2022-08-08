4e5fe4825fbd2e38ee9c8a99446f9f3393f0e95d
Gayuh kautaman <gkoma444@gmail.com>
2022-08-08 16:11:08 +0700

    added teams

--- pages/Admin/Login.tsx
+++ pages/Admin/Login.tsx
@@ -6,6 +6,7 @@ import { Formik } from "formik";
 import * as Yup from "yup";
 import fetch from "isomorphic-unfetch";
 import { login } from "../../utils/auth";
+import g from "../../styles/Glassmorphism.module.css";
 
 export default function Login() {
   const [loading, setLoading]: any = useState(false);
@@ -61,85 +62,87 @@ export default function Login() {
           <form
             noValidate
             onSubmit={formik.handleSubmit}
-            className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-xl bg-primary px-20 py-10 text-secondary"
+            className={`${g.glassHeader} absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-forth `}
           >
-            <h2 className="mb-5 text-center text-secondary">Admin</h2>
-            <section className="flex flex-col gap-1">
-              <label>Username</label>
-              <input
-                name="username"
-                id="username"
-                aria-describedby="at-sign"
-                type="text"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.username}
-                className="text-primary"
-                // invalid={formik.touched.username && !!formik.errors.username}
-              />
-              {formik.errors.username !==
-                "Do not display this error message" && (
-                <p>{formik.errors.username}</p>
-              )}
-              <label>Password</label>
-              <input
-                name="password"
-                id="password"
-                type="password"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.password}
-                className="text-primary"
-                // invalid={formik.touched.password && !!formik.errors.password}
-              />
-              {formik.errors.password !==
-                "Do not display this error message" && (
-                <p>{formik.errors.password}</p>
-              )}
-            </section>
-
-            {/* <section >
-              <Label >
-                <Input
-                  name="remember"
-                  type="checkbox"
+            <div className=" px-20 py-10">
+              <h2 className="mb-5 text-center text-forth">Admin</h2>
+              <section className="flex flex-col gap-1">
+                <label>Username</label>
+                <input
+                  name="username"
+                  id="username"
+                  aria-describedby="at-sign"
+                  type="text"
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
-                  value={formik.values.remember}
-                />{' '}
-                Remember me
+                  value={formik.values.username}
+                  className="text-primary"
+                  // invalid={formik.touched.username && !!formik.errors.username}
+                />
+                {/* {formik.errors.username !==
+                "Do not display this error message" && (
+                  <p>{formik.errors.username}</p>
+                )} */}
+                <label>Password</label>
+                <input
+                  name="password"
+                  id="password"
+                  type="password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                  className="text-primary"
+                  // invalid={formik.touched.password && !!formik.errors.password}
+                />
+                {/* {formik.errors.password !==
+                "Do not display this error message" && (
+                  <p>{formik.errors.password}</p>
+                )} */}
+              </section>
+
+              {/* <section >
+              <Label >
+              <Input
+              name="remember"
+              type="checkbox"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.remember}
+              />{' '}
+              Remember me
               </Label>
             </section> */}
 
-            <div>
-              <div className="update mx-auto">
-                <button
-                  className="my-2 rounded-lg bg-secondary px-5 py-2 text-primary"
-                  color="primary"
-                  type="submit"
-                  disabled={loading}
-                >
-                  Log In
-                  {/* {loading ? <div size="sm" /> : 'Log in'} */}
-                </button>
-              </div>
-            </div>
-
-            {networkErrors && (
               <div>
-                <div className="text-danger mx-auto">
-                  One of us is experiencing network errors 😞
+                <div className="update mx-auto">
+                  <button
+                    className="my-2 rounded-md bg-sec px-5 py-1 hover:bg-opacity-60 duration-300 text-primary"
+                    color="primary"
+                    type="submit"
+                    disabled={loading}
+                  >
+                    Log In
+                    {/* {loading ? <div size="sm" /> : 'Log in'} */}
+                  </button>
                 </div>
               </div>
-            )}
 
-            <div>
-              <div className="update mx-auto mb-2">
-                Don't have an account yet?{" "}
-                <Link href="/Admin/Register">
-                  <a>Register</a>
-                </Link>
-                .
+              {networkErrors && (
+                <div>
+                  <div className="text-danger mx-auto">
+                    One of us is experiencing network errors 😞
+                  </div>
+                </div>
+              )}
+
+              <div>
+                <div className="update mx-auto mb-2">
+                  Dont have an account yet?{" "}
+                  <Link href="/Admin/Register">
+                    <a>Register</a>
+                  </Link>
+                  .
+                </div>
               </div>
             </div>
           </form>
--- pages/Team.tsx
+++ pages/Team.tsx
@@ -1,10 +1,75 @@
 import Hero from "../components/Hero";
 import team from "../images/team/team.png";
+import Image from "next/image";
+import hari from "../images/team/hari.jpg";
+import gayuh from "../images/team/gayuh.jpg";
+import afif from "../images/team/afif.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faGoogle,
+  faYahoo,
+} from "@fortawesome/free-brands-svg-icons";
 const Team = () => {
   return (
     <div className="layout">
-      <div className="h-screen mt-2">
+      <div className="h-screen overflow-visible mt-2">
         <Hero content="" title="Team" image={team} />
+        <div className="border-x-2 h-1/6 border-forth border-opacity-60 grid md:grid-cols-3 grid-cols-1 md:w-full w-11/12 mx-auto ">
+          <div className="md:col-start-2  md:border-x-2 border-forth border-opacity-60 relative ">
+            <h1 className="text-4xl text-center font-sec font-thin whitespace-nowrap text-forth z-10 absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
+              Chief Executive Officer
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="text-forth border-x-2 border-t-2 border-forth border-opacity-60  flex flex-col">
+        <div className="m-auto z-0 p-10 w-max border-x-2 border-forth border-opacity-60">
+          <Image alt={"hari"} src={hari} width={350} height={350} />
+        </div>
+        <div className="relative ">
+          <h3 className="absolute left-1/2 -translate-x-1/2 bottom-0 z-10 ">
+            Hari Restu Septi Aji
+          </h3>
+        </div>
+        <div className="flex flex-row justify-center gap-0 border-y-2 border-forth border-opacity-60">
+          <div className="flex flex-row ">
+            <div className="basis-1/12 m-auto">
+              <h3
+                className="rotate-180 font-sec font-thin text-center "
+                style={{ writingMode: "vertical-rl" }}
+              >
+                Chief Marketing Officer
+              </h3>
+            </div>
+            <div className="basis-11/12 p-10 w-max border-l-2 border-forth border-opacity-60 relative">
+              <div className=" ">
+                <Image alt={"afif"} src={afif} width={350} height={350} />
+              </div>
+              <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
+                <h3>Afif Bareski</h3>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row">
+            <div className="basis-11/12 p-10 w-max border-x-2 border-forth border-opacity-60 relative">
+              <div className="">
+                <Image alt={"gayuh"} src={gayuh} width={350} height={350} />
+              </div>
+              <h3 className="absolute left-1/2 -translate-x-1/2 bottom-0 w-max">
+                Gayuh Kautaman
+              </h3>
+            </div>
+            <div className="basis-1/12 m-auto ">
+              <h3
+                className="rotate-180 font-thin font-sec"
+                style={{ writingMode: "vertical-rl" }}
+              >
+                Chief Technology Officer
+              </h3>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
